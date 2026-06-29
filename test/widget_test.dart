092e07ca5c86b7ba112572c1ae7d70e29a80bef9
Frank xi <yamazake@yeah.net>
2026-06-29 10:45:10 +0800

    fix: wrap CibeiApp in ProviderScope and correct finder in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:cibei_space/app.dart';
 
 void main() {
   testWidgets('App scaffold smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const CibeiApp());
-    expect(find.text('Cibei Space'), findsOneWidget);
+    await tester.pumpWidget(const ProviderScope(child: CibeiApp()));
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
